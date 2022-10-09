2692336f28904659736dd22a27b42e4f9c97ebca
Nyenskans <katarina.susic049@gmail.com>
2022-10-10 01:33:50 +0200

    2nd commit

--- com/company/Main.java
+++ com/company/Main.java
@@ -1,7 +1,7 @@
 package com.company;
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println("Hello world!");
+        System.out.println("Hello !");
 Thread.sleep(2000);
     }
 }
